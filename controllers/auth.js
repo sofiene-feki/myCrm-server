3c2ff7ab97478053b4f8ab2f1bc2c42d4ba6f1df
sofiene-feki <sofian.feki@gmail.com>
2024-10-31 12:35:30 +0100

    user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const admin = require('../firebase');
 
 exports.createOrUpdateUser = async (req, res) => {
   const { name, email } = req.user;
@@ -18,15 +19,62 @@ exports.createOrUpdateUser = async (req, res) => {
 };
 
 exports.currentUser = async (req, res) => {
-    try {
-      const currentUser = await User.findOne({ email: req.user.email }).exec();
-      res.json(currentUser);
-    } catch (err) {
-      // handle error
-    }
-  };
+  try {
+    const currentUser = await User.findOne({ email: req.user.email }).exec();
+    res.json(currentUser);
+  } catch (err) {
+    // handle error
+  }
+};
 
 exports.usersList = async (req, res) => {
   res.json(await User.find({}).sort({ createdAt: -1 }).exec());
   console.log(res);
+};
+
+// userController.js
+
+exports.createUser = async (req, res) => {
+  const { email, password, displayName, phoneNumber } = req.body;
+
+  try {
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+      displayName,
+      phoneNumber,
+      disabled: false,
+    });
+    res.status(201).json({
+      message: 'User created successfully',
+      userId: userRecord.uid,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error creating user',
+      error: error.message,
+    });
+  }
+};
+
+// userController.js
+exports.updateUser = async (req, res) => {
+  const { userId, email, displayName, phoneNumber } = req.body;
+
+  try {
+    const userRecord = await admin.auth().updateUser(userId, {
+      email,
+      displayName,
+      phoneNumber,
+    });
+    res.status(200).json({
+      message: 'User updated successfully',
+      userId: userRecord.uid,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error updating user',
+      error: error.message,
+    });
+  }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -10,12 +10,15 @@ const {
   createOrUpdateUser,
   currentUser,
   usersList,
+  createUser,
+  updateUser,
 } = require('../controllers/auth');
-
 
 router.get('/users', usersList);
 router.post('/create-or-update-user', authCheck, createOrUpdateUser);
 router.post('/current-user', authCheck, currentUser);
 router.post('/current-admin', authCheck, adminCheck, currentUser);
+router.post('/create-user', createUser);
+router.put('/update-user/:userId', updateUser);
 
-module.exports = router;  
+module.exports = router;
